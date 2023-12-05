e4dd18f298a65bb066cea69957e71bbaa256b988
Ajay Kumar <113761737+AjayRider@users.noreply.github.com>
2023-12-05 12:02:32 +0530

    Add project files.

---
+++ Controllers/MainController.cs
@@ -0,0 +1,115 @@
+﻿using ESCMS.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESCMS.Controllers
+{
+    public class MainController : Controller
+    {
+         
+        DbHelper objProduct = new DbHelper();
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<ProductModel> lstProduct = new List<ProductModel>();
+            lstProduct = objProduct.GetAllProducts().ToList();
+
+            return View(lstProduct);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind] ProductModel product)
+        {
+            if (ModelState.IsValid)
+            {
+                objProduct.AddProduct(product);
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? ProductId)
+        {
+            if (ProductId == null)
+            {
+                return NotFound();
+            }
+            ProductModel product = objProduct.GetProductData(ProductId);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int ProductId, [Bind] ProductModel productmodel)
+        {
+            if (ProductId!= productmodel.ProductId)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                objProduct.UpdateProduct(productmodel);
+                return RedirectToAction("Index");
+            }
+            return View(productmodel);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int? ProductId)
+        {
+            if (ProductId == null)
+            {
+                return NotFound();
+            }
+            ProductModel productmodel = objProduct.GetProductData(ProductId);
+
+            if (productmodel == null)
+            {
+                return NotFound();
+            }
+            return View(productmodel);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            ProductModel productmodel= objProduct.GetProductData(id);
+
+            if (productmodel == null)
+            {
+                return NotFound();
+            }
+            return View(productmodel);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int? ProductId)
+        {
+            objProduct.DeleteEmployee(ProductId);
+            return RedirectToAction("Index");
+        }
+    }
+}
+
---
+++ Models/DbHelper.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using ESCMS.Models;
+
+namespace ESCMS.Models
+{
+    public class DbHelper
+    {
+        string connectionString = "Integrated Security=true;Persist Security Info=False;Initial Catalog=ESCMS2;Data Source=DESKTOP-GOPM6K4\\SQLEXPRESS";
+
+        //To View all employees details  
+        public IEnumerable<ProductModel> GetAllProducts()
+        {
+            List<ProductModel> lstproduct = new List<ProductModel>();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("GetProducts", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    ProductModel product = new ProductModel();
+
+                    product.ProductId = Convert.ToInt32(rdr["ProductId"]);
+                    product.ProductName = rdr["ProductName"].ToString();
+                   
+
+                    lstproduct.Add(product);
+                }
+                con.Close();
+            }
+            return lstproduct;
+        }
+
+        //To Add new Product record  
+        public void AddProduct(ProductModel productModel)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("InsertProduct", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@ProductId", productModel.ProductId);
+
+                cmd.Parameters.AddWithValue("@ProductName", productModel.ProductName);
+               
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        //To Update the records of a particluar product
+        public void UpdateEmployee(ProductModel productModel)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("UpdateProduct", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@ProductId", productModel.ProductId);
+                cmd.Parameters.AddWithValue("@ProductName", productModel.ProductName);
+               
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        //Get the details of a particular product
+        public ProductModel GetEmployeeData(int? id)
+        {
+            ProductModel employee = new ProductModel();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string sqlQuery = "SELECT * FROM tblEmployee WHERE EmployeeID= " + id;
+                SqlCommand cmd = new SqlCommand(sqlQuery, con);
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    employee.ProductId = Convert.ToInt32(rdr["ProductId"]);
+                    employee.ProductName = rdr["ProductName"].ToString();
+                   
+                }
+            }
+            return employee;
+        }
+
+        //To Delete the record on a particular Product
+        public void DeleteEmployee(int? ProductId)
+        {
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("DeleteProduct", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@ProductId", ProductId);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        internal void UpdateProduct(ProductModel productmodel)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal ProductModel GetProductData(int? productId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
